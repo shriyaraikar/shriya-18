848b2d5295aa621dc3dd483df643b883b362f68a
shriyaraikar <45267427+shriyaraikar@users.noreply.github.com>
2021-08-25 15:33:13 +0530

    Add files via upload

---
+++ Payroll-Manangement-System-master/config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const mysql = require('mysql');
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://localhost/db_project');  //connect db
+const db = mongoose.connection;  //acquire the connection
+
+
+
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Success db');
+});
+
+
+
+
+
